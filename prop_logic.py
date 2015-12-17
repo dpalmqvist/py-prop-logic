ae71ae26bc6a764ea9bdb0d3bb06697cc9503ebc
Daniel Palmqvist <daniel.u.palmqvist@gmail.com>
2015-12-17 21:05:21 +0100

    Moved description into README file

--- prop_logic.py
+++ prop_logic.py
@@ -1,22 +1,5 @@
 #prop_logic.py
-#Functions, definitions and classes to manage propositional logic
-#Based on pseudo code from Artificial Intelligence a Modern Approach by Russel and Norvig
-#
-#FolKB
-#A FolKB is a knowledge base that can contain Horn-clauses.
-#FolKB implements
-# - tell(clause), this stores clause in the knowledge base
-# - ask(clause), this asks if clause is entailed by what is already stored (telled) to the knowledge base.
-# - retract(clause), removes clause from the knowledge base
-#
-#If clause is a string, it tries to create an Expr by calling make_expr
-#FolKB can handle two types of clauses:
-#- Facts: F(a,b,c...)
-#- Rules: A & B & C & ... -> D
-#NOTE: Variables are lower case, constants are upper case (reverse from Prolog)
-#- Can handle negation as failure with the special keyword Not(...)
-#See examples by running this with "python prop_logic.py"
-#
+
 #Copyright (C) 2011 Ulf Daniel Palmqvist
 #This program is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
